1d8e2da58fc85e51c2bb89dd40a002ab291e9c1c
Alex <36776298+Variiuz@users.noreply.github.com>
2021-03-07 17:14:34 +0100

    Fixed some issues in .php 1

--- examples/php/1-fetch-character.php
+++ examples/php/1-fetch-character.php
@@ -1,16 +1,16 @@
 <?php
-    $apiToken = '<token>';
+    
     function getCharacterByName($name){
-        $response = request('https://api.playafterfall.com/v3/api/search/character/name/'+$name);
+        $response = request('https://api.playafterfall.com/v3/api/search/character/name/' . $name);
         $responseArray = json_decode($response);
-        $title = request('https://api.playafterfall.com/v3/api/title/'+$responseArray['title_id']);
+        $title = request('https://api.playafterfall.com/v3/api/title/' . $responseArray['title_id']);
 
     }
     
     function request($url){
         $headers = [
             'Accept: application/json',
-            'Authorization: Bearer ' . $apiToken
+            'Authorization: Bearer <token>' // Add your Token here!
         ];
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
